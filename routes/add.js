dfe749c4a57d985d1e05d662d3f122bcc68495f2
Xiaokun Li <xil109@ucsd.edu>
2016-02-04 02:06:35 -0800

    putting it together lab

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,9 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(req, res) {  
 	// Your code goes here
- }
+    var newFriend = {"name": req.query.name, "description": req.query.description, "imageURL": "http://lorempixel.com/400/400/people"}
+    console.log("name is: " + newFriend.name + " " + "description is: " + newFriend.description);
+	data["friends"].push(newFriend);
+	res.render('add',data);
+}
